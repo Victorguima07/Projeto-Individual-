3aed74137c5919ae61229818f09028a4c6b77aba
Victorguima07 <victor.gsouza@sptech.school.com>
2025-11-16 23:15:39 -0300

    Feito alterações no index

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -4,7 +4,6 @@ function validarSessao() {
     var nome = sessionStorage.NOME_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
-    var b_cpf = document.getElementById("b_cpf");
 
     if (email != null && nome != null) {
         b_usuario.innerHTML = nome;
